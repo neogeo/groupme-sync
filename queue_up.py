6556cdff23ac6cdd2a07e17326abc5c71c1d8a59
Andrew Ozor <andrew.ozor@web.com>
2017-08-10 14:30:04 -0500

    Get All Messages for a Group
    Created a method to incrementally get all messages for a group, starting from the most recent. For this purpose, we only care about messages with pictures or video attached.
    
    The next step will be saving the relevant events to firebase.

--- queue_up.py
+++ queue_up.py
@@ -10,6 +10,7 @@ HEADERS = {
 }
 GROUP_NAME = 'YOUR_GROUP_NAME'
 GROUP_ID = 'YOUR_GROUP_ID'
+GROUPME_NO_MESSAGE_STATUS_CODE = 304
 
 logger = logging.getLogger(__name__)
 
@@ -33,6 +34,44 @@ def get_messages(limit=100):
     res.raise_for_status()
 
     return res.json()['response']['messages']
+
+
+def get_messages_before(groupme_message_id):
+    '''Gets 100 messages that occurred immediately before the given id
+    This will return an empty list if no messages exist before
+    '''
+    limit = 100
+    url = '{}/groups/{}/messages'.format(GROUPME_URL, GROUP_ID)
+    res = requests.get(url, headers=HEADERS, params={'limit': limit, 'before_id': groupme_message_id})
+
+    if res.status_code == GROUPME_NO_MESSAGE_STATUS_CODE:
+        logging.info('no more messages before {}'.format(groupme_message_id))
+        return []
+
+    res.raise_for_status()
+
+    return res.json()['response']['messages']
+
+
+def get_all_messages_iter():
+    '''Gets all messages in descending order of date created
+    '''
+    # Get most recent message becuase it's ID is needed to paginate backwards in time
+    most_recent_message = get_messages(1)[0]
+    yield most_recent_message
+
+    oldest_message_id = most_recent_message['id']
+    messages = get_messages_before(oldest_message_id)
+
+    while messages:
+        logger.info('found {} messages, from {} to {}'.format(len(messages), arrow.get(messages[1]['created_at']), arrow.get(messages[-1]['created_at'])))
+        for message in messages:
+            yield message
+
+        oldest_message_id = messages[-1]
+        messages = get_messages_before(oldest_message_id)
+
+    logger.info('finished fetching all messages')
 
 
 def create_event_from_groupme_message(message):
@@ -67,20 +106,18 @@ def verfify_fishy_group_exists():
     raise Exception('working with invalid group {}'.format(groups))
 
 
-# TODO: yeild, and paginate all messages, and log
 def get_all_multi_media_messages():
     total_count = 0
-    limit = 2
-    messages = get_messages(limit)
 
-    for message in messages:
-        # if the message has a pic or video
+    for message in get_all_messages_iter():
+        # only interested in messages with a picture or video
         if message['attachments']:
             event_obj = create_event_from_groupme_message(message)
-            print(event_obj)
             total_count += 1
 
-    print(total_count)
+        # TODO: save events in chunks of 100
+
+    logger.info('finished saving {} events')
 
 
 if __name__ == '__main__':
